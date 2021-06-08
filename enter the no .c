ae83032dfe3598b060038602e7401d06fbbbfadf
Sebin ms <sebinkelakam@gmail.com>
2021-06-08 13:00:04 +0500

    Add files via upload

---
+++ enter the no .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,i=1;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    do
+    {
+        printf("%d \n",i);
+        i++;
+    }
+    while(i<=n);
+
+}
+
+
+
