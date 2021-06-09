daf70bbceba36debeea41cceccfd94f5c2a59ee1
Sebin ms <sebinkelakam@gmail.com>
2021-06-09 09:20:35 +0500

    Add files via upload

---
+++ print any numbers(do while).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int n,i=1;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    do
+    {
+        printf("%d \n",i);
+        i++;
+    }
+
+
+
+    while(i<=n);
+
+}
+
+
+
